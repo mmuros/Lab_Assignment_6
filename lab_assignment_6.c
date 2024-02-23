6f0eacc466810f1f381e469edbf7b157e8247988
mmuros <160198232+mmuros@users.noreply.github.com>
2024-02-22 20:43:22 -0500

    Update lab_assignment_6.c

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -17,13 +17,6 @@ int search(int numbers[], int low, int high, int value) {
     }
 }
 
-#include <stdio.h>
-
-int search(int numbers[], int low, int high, int value) 
-{
-	return -1;
-}
-
 void printArray(int numbers[], int sz)
 {
 	int i;
